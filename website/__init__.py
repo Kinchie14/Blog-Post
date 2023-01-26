bebe312dbcac3de97544a09828fbfe5015d81403
Vincent Vargas <vincentvargas6969@gmail.com>
2023-01-26 05:13:09 -0800

    TANGINA -_-

--- website/__init__.py
+++ website/__init__.py
@@ -7,8 +7,6 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
-
-
 app.config['SECRET_KEY'] = 'mysecretkey'
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
@@ -16,14 +14,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Migrate(app,db)
 
-from website.core.views import core
-from website.users.views import users
-from website.errorpage.errorhandler import error_pages
-
-app.register_blueprint(core, url_prefix='/')
-app.register_blueprint(users, url_prefix='/')
-app.register_blueprint(error_pages, url_prefix='/')
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'users.login'
+
+
+from website.core.views import core
+from website.users.views import users
+from website.errorpage.errorhandler import error_pages
+
+app.register_blueprint(core)
+app.register_blueprint(users)
+app.register_blueprint(error_pages)
