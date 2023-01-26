bf1f2af10f1a54ee60363ceeedc64341e210dd35
Vincent Vargas <vincentvargas6969@gmail.com>
2023-01-26 00:17:43 -0800

    some error handler

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from website.core.views import core
 from website.users.views import users
+from website.errorpage.errorhandler import error_pages
 
 
 
@@ -10,3 +11,4 @@ app = Flask(__name__)
 
 app.register_blueprint(core, url_prefix='/')
 app.register_blueprint(users, url_prefix='/')
+app.register_blueprint(error_pages, url_prefix='/')
